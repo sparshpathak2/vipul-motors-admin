35d3e1f6acffd63e6a574c79250926f353ee36ee
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2025-08-12 09:40:04 +0530

    Fix for client side error on production

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client"
 
-import { redirect } from "next/navigation"
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-  redirect('/queries')
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/queries')
+  }, [router])
+
+  return null
 }
